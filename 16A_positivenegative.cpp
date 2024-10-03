64e06a240f7b05a2ba28b8f33f3ef28bf6eec204
mukeshrothe13 <rothemukesh7@gmail.com>
2024-10-03 21:24:00 +0530

    16A_positivenegative.cpp

---
+++ 16A_positivenegative.cpp
@@ -0,0 +1,21 @@
+//Mukesh Rothe 23070123089
+# include<iostream>
+using namespace std;
+ int main()
+ {  float a;
+    cout<< "Enter a positive  number: "<<endl;
+    cin>>a;
+try {
+    if ( a<0)
+    {
+        throw a;
+    }
+else {
+ cout<< "The number  is: "<<a<<endl;
+ }
+}
+catch (const float n)
+{
+    cout<<"You entered " <<a<<" which is a negative number";
+}
+ }
